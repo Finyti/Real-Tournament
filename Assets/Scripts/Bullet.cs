cc902825c70b8c86a874c2bc22113fa6c6a262a0
Finyti <122672171+Finyti@users.noreply.github.com>
2024-02-21 18:47:09 +0200

    Effects, bounces, bullet-holes

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,12 @@ public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public int damage = 1;
+    public GameObject destroyPrefab;
+    public GameObject flyPrefab;
+    public GameObject hitPrefab;
+
+    public int bounceCount = 3;
+
     void Start()
     {
         Destroy(gameObject, 3f);
@@ -14,11 +20,27 @@ public class Bullet : MonoBehaviour
     void Update()
     {
         print("F");
+        Instantiate(flyPrefab, transform.position, Quaternion.identity);
         transform.position += transform.forward * speed * Time.deltaTime;
     }
 
     void OnCollisionEnter(Collision other)
     {
-        Destroy(gameObject);
+        if(bounceCount > 0)
+        {
+            transform.forward = other.contacts[0].normal;
+
+            var hole = Instantiate(hitPrefab, transform.position, transform.rotation);
+
+            bounceCount--;
+        }
+        else
+        {
+            Instantiate(destroyPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+
+
     }
 }
